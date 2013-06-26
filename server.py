bed11980e7c53078f43fa5acd690e9468d22b529
Daniel Shim <shimdh@gmail.com>
2013-06-26 22:32:11 +0900

    Reformatted.

--- server.py
+++ server.py
@@ -6,5 +6,7 @@ from tornado.ioloop import IOLoop
 from mrd_srv import app
 
 http_server = HTTPServer(WSGIContainer(app))
+# http_server.bind(5000)
+# http_server.start(0)
 http_server.listen(5000)
 IOLoop.instance().start()
