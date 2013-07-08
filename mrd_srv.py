49c503c753b52a483050258ae7ec20a980ce84da
Daniel Shim <shimdh@gmail.com>
2013-07-08 17:43:15 +0900

    Reformatted.

--- mrd_srv.py
+++ mrd_srv.py
@@ -33,57 +33,70 @@ def index():
 
 app.add_url_rule(
     '/register', 'register', 
-    view_users.register
-)
+    view_users.register)
 app.add_url_rule(
     '/login', 'login', 
-    view_users.login
-)
+    view_users.login)
 app.add_url_rule(
     '/create_character', 'create_character', 
-    view_characters.createCharacter
-)
+    view_characters.createCharacter)
 app.add_url_rule(
     '/get_notice', 'get_notice', 
-    view_infos.getNotice
-)
+    view_infos.getNotice)
 app.add_url_rule(
-    '/get_character', 'get_character', view_characters.getCharacter)
+    '/get_character', 'get_character', 
+    view_characters.getCharacter)
 app.add_url_rule(
-    '/check_gameversion', 'check_gameversion', view_infos.checkGameVersion)
+    '/check_gameversion', 'check_gameversion', 
+    view_infos.checkGameVersion)
 app.add_url_rule(
-    '/add_friend', 'add_friend', view_friends.addFriend)
+    '/add_friend', 'add_friend', 
+    view_friends.addFriend)
 app.add_url_rule(
-    '/get_friendslist', 'get_friendslist', view_friends.getFriendsList)
+    '/get_friendslist', 'get_friendslist', 
+    view_friends.getFriendsList)
 app.add_url_rule(
     '/find_friendbynickname', 'find_friendbynickname',
     view_friends.findFriendByName)
 app.add_url_rule(
-    '/get_inventories', 'get_inventories', view_inventories.getInventories)
+    '/get_inventories', 'get_inventories', 
+    view_inventories.getInventories)
 app.add_url_rule(
-    '/set_inventories', 'set_inventories', view_inventories.setInventories)
+    '/set_inventories', 'set_inventories', 
+    view_inventories.setInventories)
 app.add_url_rule(
-    '/set_slots', 'set_slots', view_slots.setSlots)
+    '/set_slots', 'set_slots', 
+    view_slots.setSlots)
 app.add_url_rule(
-    '/get_slots', 'get_slots', view_slots.getSlots)
+    '/get_slots', 'get_slots', 
+    view_slots.getSlots)
 app.add_url_rule(
-    '/set_stats', 'set_stats', view_stats.setStats)
+    '/set_stats', 'set_stats', 
+    view_stats.setStats)
 app.add_url_rule(
-    '/get_stats', 'get_stats', view_stats.getStats)
+    '/get_stats', 'get_stats', 
+    view_stats.getStats)
 app.add_url_rule(
-    '/write_mail', 'write_mail', view_mails.writeMail)
+    '/write_mail', 'write_mail', 
+    view_mails.writeMail)
 app.add_url_rule(
-    '/read_mail', 'read_mail', view_mails.readMail)
+    '/read_mail', 'read_mail', 
+    view_mails.readMail)
 app.add_url_rule(
-    '/get_giftmail', 'get_giftmail', view_mails.getGiftMail)
+    '/get_giftmail', 'get_giftmail', 
+    view_mails.getGiftMail)
 app.add_url_rule(
-    '/get_maillist', 'get_maillist', view_mails.getMailList)
+    '/get_maillist', 'get_maillist', 
+    view_mails.getMailList)
 app.add_url_rule(
-    '/delete_mails', 'delete_mails', view_mails.deleteMails)
+    '/delete_mails', 'delete_mails', 
+    view_mails.deleteMails)
 app.add_url_rule(
-    '/set_owncostumes', 'set_owncostumes', view_costumes.setOwnCostumes)
+    '/set_owncostumes', 'set_owncostumes', 
+    view_costumes.setOwnCostumes)
 app.add_url_rule(
-    '/get_owncostumes', 'get_owncostumes', view_costumes.getOwnCostumes)
+    '/get_owncostumes', 'get_owncostumes', 
+    view_costumes.getOwnCostumes)
 app.add_url_rule(
     '/set_owncostumebases', 'set_owncostumebases',
     view_costumes.setOwnCostumebases)
@@ -91,21 +104,26 @@ app.add_url_rule(
     '/get_owncostumebases', 'get_owncostumebases',
     view_costumes.getOwnCostumebases)
 app.add_url_rule(
-    '/request_friend', 'request_friend', view_friends.requestFriend)
+    '/request_friend', 'request_friend', 
+    view_friends.requestFriend)
 app.add_url_rule(
     '/get_waitingfriends', 'get_waitingfriends',
     view_friends.getWaitingFriends)
 app.add_url_rule(
-    '/accept_friend', 'accept_friend', view_friends.acceptFriend)
+    '/accept_friend', 'accept_friend', 
+    view_friends.acceptFriend)
 app.add_url_rule(
     '/get_friendcharacterinfo', 'get_friendcharacterinfo',
     view_friends.getFriendCharacterInfo)
 app.add_url_rule(
-    '/add_completedevent', 'add_completedevent', view_events.addCompletedEvent)
+    '/add_completedevent', 'add_completedevent', 
+    view_events.addCompletedEvent)
 app.add_url_rule(
-    '/get_fishing', 'get_fishing', view_fishing.getFishing)
+    '/get_fishing', 'get_fishing', 
+    view_fishing.getFishing)
 app.add_url_rule(
-    '/add_owncostume', 'add_owncostume', view_costumes.addOwnCostume)
+    '/add_owncostume', 'add_owncostume', 
+    view_costumes.addOwnCostume)
 app.add_url_rule(
     '/add_owncostumebase', 'add_owncostumebase',
     view_costumes.addOwnCostumeBase)
@@ -116,13 +134,17 @@ app.add_url_rule(
     '/receive_friendshippoint', 'receive_friendshippoint',
     view_friends.receiveFriendShipPoint)
 app.add_url_rule(
-    '/set_buttonstate', 'set_buttonstate', view_saved.setButtonState)
+    '/set_buttonstate', 'set_buttonstate', 
+    view_saved.setButtonState)
 app.add_url_rule(
-    '/get_buttonstate', 'get_buttonstate', view_saved.getButtonState)
+    '/get_buttonstate', 'get_buttonstate', 
+    view_saved.getButtonState)
 app.add_url_rule(
-    '/set_savedstory', 'set_savedstory', view_saved.setSavedStory)
+    '/set_savedstory', 'set_savedstory', 
+    view_saved.setSavedStory)
 app.add_url_rule(
-    '/get_savedstory', 'get_savedstory', view_saved.getSavedStory)
+    '/get_savedstory', 'get_savedstory', 
+    view_saved.getSavedStory)
 app.add_url_rule(
     '/set_savedcurrentzone', 'set_savedcurrentzone',
     view_saved.setSavedCurrentZone)
@@ -130,25 +152,35 @@ app.add_url_rule(
     '/get_savedcurrentzone', 'get_savedcurrentzone',
     view_saved.getSavedCurrentZone)
 app.add_url_rule(
-    '/add_puzzlepiece', 'add_puzzlepiece', view_puzzles.addOpenedPuzzlePiece)
+    '/add_puzzlepiece', 'add_puzzlepiece', 
+    view_puzzles.addOpenedPuzzlePiece)
 app.add_url_rule(
-    '/get_puzzlepieces', 'get_puzzlepieces', view_puzzles.getOpenedPuzzlePieces)
+    '/get_puzzlepieces', 'get_puzzlepieces', 
+    view_puzzles.getOpenedPuzzlePieces)
 app.add_url_rule(
-    '/add_puzzle', 'add_puzzle', view_puzzles.addOpenedPuzzle)
+    '/add_puzzle', 'add_puzzle', 
+    view_puzzles.addOpenedPuzzle)
 app.add_url_rule(
-    '/get_puzzles', 'get_puzzles', view_puzzles.getOpenedPuzzles)
+    '/get_puzzles', 'get_puzzles', 
+    view_puzzles.getOpenedPuzzles)
 app.add_url_rule(
-    '/add_diary', 'add_diary', view_diaries.addDiary)
+    '/add_diary', 'add_diary', 
+    view_diaries.addDiary)
 app.add_url_rule(
-    '/get_diaries', 'get_diaries', view_diaries.getDiaries)
+    '/get_diaries', 'get_diaries', 
+    view_diaries.getDiaries)
 app.add_url_rule(
-    '/set_worncostume', 'set_worncostume', view_costumes.setWornCostume)
+    '/set_worncostume', 'set_worncostume', 
+    view_costumes.setWornCostume)
 app.add_url_rule(
-    '/get_worncostume', 'get_worncostume', view_costumes.getWornCostume)
+    '/get_worncostume', 'get_worncostume', 
+    view_costumes.getWornCostume)
 app.add_url_rule(
-    '/set_cash', 'set_cash', view_users.setCash)
+    '/set_cash', 'set_cash', 
+    view_users.setCash)
 app.add_url_rule(
-    '/get_cash', 'get_cash', view_users.getCash)
+    '/get_cash', 'get_cash', 
+    view_users.getCash)
 
 @app.route('/hello')
 def hello():
