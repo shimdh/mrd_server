e65420a000a3edb8d90565756d4c3972e5b28fda
Daniel Shim <shimdh@gmail.com>
2013-07-12 10:21:47 +0900

    Reformatted.

--- view_saved.py
+++ view_saved.py
@@ -104,7 +104,10 @@ def setSavedStory():
                     db_session.add(find_story)
                 else:
                     made_story = SavedStory(
-                        got_user.id, got_data['zone_index'], got_data['episode_no'], got_data['wave_no'])
+                        got_user.id, 
+                        got_data['zone_index'], 
+                        got_data['episode_no'], 
+                        got_data['wave_no'])
                     made_story.position = json.dumps(got_data['position'])
                     made_story.rotation = json.dumps(got_data['rotation'])
 
