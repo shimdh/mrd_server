eb3d026d5981b89c9b8ffdbf4d3adb32fecfdaab
Daniel, Shim <shimdh@gmail.com>
2013-08-05 17:25:51 +0900

    Modified again.

--- utils.py
+++ utils.py
@@ -55,8 +55,7 @@ def checkSessionId(got_session_id):
         return ResultCodes.SessionIdNonExist, None
     else:
         if got_user.session_date < (
-                #datetime.datetime.now() - datetime.timedelta(minutes=20)):
-                datetime.datetime.now() - datetime.timedelta(minutes=1)):
+                datetime.datetime.now() - datetime.timedelta(minutes=20)):
             got_user.session_id = ''
             db_session.add(got_user)
             try:
